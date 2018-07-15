49b7a7864dc4af47963176a84f52fc6507f43685
EC2 Default User <ec2-user@ip-172-31-54-171.ec2.internal>
2018-07-15 15:56:51 +0000

    Saving changes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,15 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ["$http", function($http) {
+  $http({
+    method: 'GET',
+    url: 'https://a6c8d249599c4708823caa8f40da103f.vfs.cloud9.us-east-1.amazonaws.com:8082/terms.json'
+  }).then(function successCallback(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
 }]);
